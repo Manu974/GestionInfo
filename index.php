5158153fa9b69f1000f3554f480f9bea36e837f9
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-03-01 17:53:29 +0100

    creation du projet de gestion

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link href="gestioninfo.css" rel="stylesheet" />
+    <title>GESTION INFORMATIQUE - Home</title>
+</head>
+<body>
+    <header>
+        <h1>GESTION</h1>
+    </header>
+    
+    <footer class="footer">
+        <a href="https://github.com/bpesquet/OC-MicroCMS">MicroCMS</a> is a minimalistic CMS built as a showcase for modern PHP development.
+    </footer>
+</body>
+</html>
